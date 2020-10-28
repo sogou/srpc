33590cf4624d7b037d5a5db062476c993b719aa6
holmes1412 <liyingxin1412@gmail.com>
2020-10-28 22:51:40 +0800

    update example

--- example/server.cc
+++ example/server.cc
@@ -75,10 +75,10 @@ static void run_server(unsigned short port)
 {
 	RPCServerParams params = RPC_SERVER_PARAMS_DEFAULT;
 	params.max_connections = 2048;
-	RPCSpanLoggerDefault RPC_SPAN_LOGGER_DEFAULT;
-	RPC_SPAN_LOGGER_DEFAULT.set_span_limit(2);
+	RPCSpanLoggerDefault span_logger;
+	span_logger.set_span_limit(2);
 
-	params.span_logger = &RPC_SPAN_LOGGER_DEFAULT;
+	params.span_logger = &span_logger;
 	SERVER server(&params);
 
 	ExamplePBServiceImpl pb_impl;
