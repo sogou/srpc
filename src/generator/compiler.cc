384fc91e833888cb5674af837eb3c2bb0c800a9b
Lance Lin <lqi254@protonmail.com>
2022-01-28 21:11:04 +0700

    Added detection of input IDL file type. Fixed crash if output directory does not exist

--- src/generator/compiler.cc
+++ src/generator/compiler.cc
@@ -21,24 +21,28 @@
 #include <unistd.h>
 #endif
 #include "generator.h"
+#include <fstream>
+
+/* LQ - prototype to determine if the file type is thrift */
+bool isThrift(char* filename);
 
 int main(int argc, const char *argv[])
 {
-	if (argc != 4)
-	{
-		fprintf(stderr, "Usage:\n\t%s protobuf proto_file output_dir\n"
-				"\t%s thrift thrift_file output_dir\n", argv[0], argv[0]);
+	/* LQ - update to use 2 command line arguments */
+	/* TODO: use optarg/getopt_long to have index-independent arguments? */
+	/* 	 Ex: srpc_generator -i <IDL_FILE> -o <OUTPUT_DIR> or */
+	/*  	     srpc_generator --input <IDL_FILE> --output <OUTPUT_DIR> */
+	if(argc != 3) {
+		fprintf(stderr, "Usage:\n\t%s <idl_file> <output_dir>\n", argv[0]);
 		return 0;
 	}
-
 	char *idl = const_cast<char *>(argv[1]);
-	bool is_thrift = (strcasecmp(idl, "thrift") == 0);
-	const char *idl_file = argv[2];
+	bool is_thrift = isThrift(idl);
+	const char *idl_file = argv[1];
 	struct GeneratorParams params;
 	Generator gen(is_thrift);
-//	Generate(file, parameter, generator_context, error);
 
-	params.out_dir = argv[3];
+	params.out_dir = argv[2];
 
 	char file_name[DIRECTORY_LENGTH] = { 0 };
 	size_t pos = 0;
@@ -59,4 +63,37 @@ int main(int argc, const char *argv[])
 	gen.generate(file_name, params);
 	fprintf(stdout, "[Generator Done]\n");
 	return 0;
+}
+
+/* LQ - function to determine if the input file is thrift or not */
+/* Date: 28 JAN 2022 */
+/* Description: Opens a file, reads the lines and checks for "syntax = proto", */
+/* 		which indicates a protobuf file. If it does not find this string, */
+/* 		return true and assume the file is thrift. Error out if file is unable to be read */
+/* Notes/TODO:  * Add enum to handle file types? (TYPE_THRIFT, TYPE_PROTOBUF, TYPE_UNKNOWN) */
+/*		* Better detection of file types? Better behaviour if thrift files have a specific format to look for */
+/*		* Handle error case when user provides a file that is not thrift nor protobuf */
+bool isThrift(char* filename) {
+
+	std::ifstream idl_file;
+	std::string line;
+	idl_file.open(filename);
+
+	if(idl_file.is_open()) {
+		while(!idl_file.eof()) {
+			getline(idl_file, line);
+			/* If the line contains "syntax = proto", it is a protobuf file */
+			if(line.find("syntax = proto") != std::string::npos) {
+				idl_file.close();
+				return false;
+			}	
+		}
+		idl_file.close();
+	} else {
+		/* Error case */
+		fprintf(stderr, "Unable to open IDL file: %s\n", filename);
+		exit(0);
+	}
+	/* Otherwise, return true (assume thrift format) */
+	return true;
 }
