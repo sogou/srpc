141ed832018508a6da6402e069884a7db2f685ea
liyingxin <liyingxin@sogou-inc.com>
2022-03-02 17:58:27 +0800

    update submodule workflow and add codes for ThriftHttpResponse

--- src/message/rpc_message_thrift.cc
+++ src/message/rpc_message_thrift.cc
@@ -242,10 +242,16 @@ bool ThriftHttpResponse::serialize_meta()
 		return false;
 
 	int rpc_status_code = this->get_status_code();
+	int http_status_code = this->get_http_code();
 
 	set_http_version("HTTP/1.1");
 	if (rpc_status_code == RPCStatusOK)
-		protocol::HttpUtil::set_response_status(this, HttpStatusOK);
+	{
+		if (http_status_code)
+			protocol::HttpUtil::set_response_status(this, http_status_code);
+		else
+			protocol::HttpUtil::set_response_status(this, HttpStatusOK);
+	}
 	else if (rpc_status_code == RPCStatusServiceNotFound
 			|| rpc_status_code == RPCStatusMethodNotFound
 			|| rpc_status_code == RPCStatusMetaError
