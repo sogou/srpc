625b86799d8f75d829800026abc870fb30aa43b0
holmes1412 <holmesyxli@tencent.com>
2023-05-19 00:52:44 +0800

    tutorial : add wait_group

--- tutorial/tutorial-06-brpc_pb_client.cc
+++ tutorial/tutorial-06-brpc_pb_client.cc
@@ -15,9 +15,12 @@
 */
 
 #include <stdio.h>
+#include "workflow/WFFacilities.h"
 #include "echo_pb.srpc.h"
 
 using namespace srpc;
+
+static WFFacilities::WaitGroup wait_group(1);
 
 int main()
 {
@@ -61,6 +64,7 @@ int main()
 		printf("status[%d] error[%d] errmsg:%s\n",
 				sync_ctx.status_code, sync_ctx.error, sync_ctx.errmsg.c_str());
 
+	wait_group.wait();
 	return 0;
 }
 
